f58ad46396b3cdce3029740d2ffc64caa4259e48
Starostina Helen <starostina.lena05@gmail.com>
2025-02-15 10:02:18 +0300

    lab1 done

---
+++ IterativeMethod.py
@@ -0,0 +1,88 @@
+class IterativeMethod:
+
+    matrix = []
+    right_matrix = []
+    n = 0
+    cur_iter_x = []
+    cur_iter_number = -1
+    new_iter_x = []
+    eps = 0.1
+    delta = []
+
+    def __init__(self, matrix, right_matrix, n, eps):
+        self.matrix = matrix
+        self.right_matrix = right_matrix
+        self.n = n
+        self.cur_iter_x = [0] * n
+        self.new_iter_x = [0] * n
+        self.eps = eps
+        self.delta = [0] * n
+        if not self.check_diagonal():
+            print('Need to permutate matrix')
+            self.permutate_matrix()
+        if not self.check_diagonal():
+            print('Diagonal elements are not dominant')
+        self.go()
+
+    def make_iteration(self):
+        self.cur_iter_number += 1
+        for i in range(self.n):
+            new_x = self.right_matrix[i] / self.matrix[i][i]
+            for j in range(self.n):
+                if j != i:
+                    new_x -= self.matrix[i][j] / self.matrix[i][i] * self.cur_iter_x[j]
+            self.new_iter_x[i] = new_x
+
+    def check_stop(self):
+        for i in range(self.n):
+            if abs(self.new_iter_x[i] - self.cur_iter_x[i]) > self.eps:
+                self.cur_iter_x = self.new_iter_x.copy()
+                return False
+            self.delta[i] = abs(self.new_iter_x[i] - self.cur_iter_x[i])
+        return True
+
+    def go(self):
+        self.make_iteration()
+        while not self.check_stop():
+            if self.cur_iter_number >= 100:
+                print('Too many iterations')
+                return
+            self.make_iteration()
+        print('Number of iterations:', self.cur_iter_number)
+        print('Result:', self.new_iter_x)
+        print('Delta:', self.delta)
+
+    def check_diagonal(self):
+        flag = False
+        for i in range(self.n):
+            summ = 0
+            for j in range(self.n):
+                if i != j:
+                    summ += abs(self.matrix[i][j])
+            if abs(self.matrix[i][i]) < summ:
+                return False
+            if abs(self.matrix[i][i]) > summ:
+                flag = True
+        return flag
+
+    def permutate_matrix(self):
+        for i in range(self.n):
+            max_el = self.matrix[i][i]
+            correct_row = i
+            for j in range(i + 1, self.n):
+                if abs(self.matrix[i][j]) > max_el:
+                    max_el = self.matrix[i][j]
+                    correct_row = j
+            self.matrix[i], self.matrix[correct_row] = self.matrix[correct_row], self.matrix[i]
+            self.right_matrix[i], self.right_matrix[correct_row] = self.right_matrix[correct_row], self.right_matrix[i]
+        print('Permutated matrix:', self.matrix)
+        print('Permutated right matrix:', self.right_matrix)
+
+    def calc_norm(self):
+        res = 0
+        for i in range(self.n):
+            summ = 0
+            for j in range(self.n):
+                summ += abs(self.matrix[i][j])
+            res = max(res, summ)
+        print('Matrix norm:', res)
---
+++ main.py
@@ -0,0 +1,75 @@
+from IterativeMethod import IterativeMethod
+
+def interact_with_user():
+    print('Hello :) Choose the preferable way of input:')
+    print('1. From file')
+    print('2. From console')
+    choice = input()
+    while choice != '1' and choice != '2':
+        print('Wrong input. Try again:')
+        choice = input()
+    eps = 0
+    n = 0
+    matrix = []
+    right_matrix = []
+    if choice == '1':
+        print('Make sure that the first line of the file is epsilon, second is the size of the matrix (<=20), then goes the matrix itself')
+        print('Example for {x1 + 2 * x2 = 7 && x1 + x2 = 5}:')
+        print(0.01, 2, '1 2 7', '1 1 5', sep='\n')
+        print('Enter the name of the file:')
+        filename = input()
+        try:
+            with open(filename) as f:
+                eps = float(f.readline())
+                n = int(f.readline())
+                if n > 20:
+                    raise ValueError
+                matrix = []
+                right_matrix = []
+                for i in range(n):
+                    line = list(map(float, f.readline().split()))
+                    if len(line) != n + 1:
+                        raise ValueError
+                    matrix.append(line[:-1])
+                    right_matrix.append(line[-1])
+        except FileNotFoundError:
+            print('File not found')
+            return
+        except ValueError or IndexError:
+            print('Data is incorrect')
+            return
+    else:
+        eps = input('Enter epsilon: ')
+        while True:
+            try:
+                eps = float(eps)
+                break
+            except ValueError:
+                print('Wrong input, try again')
+                eps = input('Enter epsilon: ')
+        n = input('Enter the size of the matrix (<=20): ')
+        while True:
+            try:
+                n = int(n)
+                if n > 20:
+                    raise ValueError
+                break
+            except ValueError:
+                print('Wrong input, try again')
+                n = input('Enter the size of the matrix: ')
+        print('Enter the matrix. Example: line should be like "1 2 7" for {x1 + 2 * x2 = 7}')
+        for i in range(n):
+            while True:
+                try:
+                    line = list(map(float, input().split()))
+                    if len(line) != n + 1:
+                        raise ValueError
+                    break
+                except ValueError:
+                    print('Wrong input, try again')
+            matrix.append(line[:-1])
+            right_matrix.append(line[-1])
+    IterativeMethod(matrix, right_matrix, n, eps)
+
+if __name__ == '__main__':
+    interact_with_user()
